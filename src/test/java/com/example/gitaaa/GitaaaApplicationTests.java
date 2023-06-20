f9c3aa75642d8b92f0d91658e0db4251fe3e380e
lonietz <844427894@qq.com>
2023-06-19 22:00:46 -0500

    aaa

---
+++ src/test/java/com/example/gitaaa/GitaaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitaaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitaaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
